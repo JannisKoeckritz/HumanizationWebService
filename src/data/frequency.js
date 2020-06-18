4e5ad594b605620fed2d289d33acc4a46a19a37d
JannisKoeckritz <jannis.koeckritz@gmx.de>
2020-06-18 12:00:05 +0200

    Update frequency data

--- src/data/frequency.js
+++ src/data/frequency.js
@@ -1,6 +1,3 @@
-import frequency_heavy from './frequency_heavy_table.json';
-import frequency_light from './frequency_light_table.json';
+import frequency from './frequency.json';
 
-export const frequency_heavy_data = {'imgt' :frequency_heavy};
-
-export const frequency_light_data = {'imgt':frequency_light};
+export const frequency_data = frequency;
